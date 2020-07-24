59ec46529c42a42a9dbcc1779840ad9d9f7da4f1
Vitalie Mudrenco <xvitcoder@gmail.com>
2020-07-24 12:26:55 +0300

    Added the install script and urxvt font resizer plugin

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+USER=xvitcoder
+HOME_DIR=/home/$USER
+
+# Install packages
+cd $HOME_DIR
+
+echo "Installing bspwm"
+pacman -Syu xorg-xinit xorg-server bspwm dmenu sxhkd nitrogen picom lightdm lightdm-gtk-greeter 
+
+echo "Installing fonts"
+pacman -Syu ttf-dejavu ttf-jetbrains-mono
+
+echo "Installing applications"
+pacman -Syu firefox rxvt-unicode pcmanfm bluez neovim tmux zsh bat htop git ack ripgrep k9s go ansible fzf helm ranger tree wget
+
+echo "Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+echo "Clonning the config repo"
+git clone https://github.com/xvitcoder/config.git
+
+echo "Applying configuration"
+cp -Rf config .config
+
+python -m pip install --user --upgrade pynvim
+
+rm .zshrc
+ln -s $HOME_DIR/.config/zsh/zshrc $HOME_DIR/.zshrc
+
+#
+ln -s $HOME_DIR/.config/tmux/tmux.conf $HOME_DIR/.tmux.conf
+ln -s $HOME_DIR/.config/nvim/init.vim $HOME_DIR/.vimrc
+ln -s $HOME_DIR/.config/ideavim/ideavimrc $HOME_DIR/.ideavim
+ln -s $HOME_DIR/.config/X/Xresources $HOME_DIR/.Xresources
+ln -s $HOME_DIR/.config/X/xinitrc $HOME_DIR/.xinitrc
+ln -s $HOME_DIR/.config/git/gitconfig $HOME_DIR/.gitconfig
+
+# Apply urxvt configuration
+mkdir -p $HOME_DIR/.urxvt/ext
+ln $HOME_DIR/.config/urxvt/ext/font-size $HOME_DIR/.urxvt/ext/font-size
+
+echo "Fixing permissions"
+chown -R $USER:$USER $HOME_DIR/*

4f8958afcb14907fbd7e2a7169938cb64fa76476
Vitalie Mudrenco <xvitcoder@gmail.com>
2020-07-26 16:40:35 +0300

    Added polybar conf

--- install.sh
+++ install.sh
@@ -7,13 +7,22 @@ HOME_DIR=/home/$USER
 cd $HOME_DIR
 
 echo "Installing bspwm"
-sudo pacman -Syu --noconfirm xorg-xinit xorg-server bspwm dmenu sxhkd nitrogen picom lightdm lightdm-gtk-greeter 
+sudo pacman -Syu --noconfirm xorg-xinit xorg-server bspwm dmenu sxhkd nitrogen picom lightdm lightdm-gtk-greeter
 
 echo "Installing fonts"
 sudo pacman -Syu --noconfirm ttf-dejavu ttf-jetbrains-mono
 
 echo "Installing applications"
-sudo pacman -Syu --noconfirm firefox vimb rxvt-unicode pcmanfm bluez bluez-utils neovim tmux zsh bat htop git ack ripgrep k9s go go-utils ansible fzf helm ranger tree wget vagrant
+sudo pacman -Syu --noconfirm firefox vimb qutebrowser pcmanfm bluez bluez-utils neovim tmux zsh bat htop git ack ripgrep k9s go go-utils ansible fzf helm ranger tree wget vagrant jdk-openjdk maven gradle libreoffice
+
+echo "Install AUR"
+sudo git clone https://aur.archlinux.org/yay-git.git
+cd yay-git
+makepkg -si
+rm -rf yay-git
+
+echo "Installing AUR packages"
+yay -Syu --noconfirm polybar
 
 echo "Installing oh-my-zsh"
 curl -Lo install_oh_my_zsh.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
@@ -22,7 +31,7 @@ rm install_oh_my_zsh.sh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 echo "Clonning the config repo"
-git clone https://github.com/xvitcoder/config.git 
+git clone https://github.com/xvitcoder/config.git
 mkdir -p $HOME_DIR/.config
 cp -rf config/* .config/
 cp -rf config/.git .config/
@@ -37,17 +46,13 @@ ln -s --relative $HOME_DIR/.config/zsh/zshrc $HOME_DIR/.zshrc
 ln -s --relative $HOME_DIR/.config/tmux/tmux.conf $HOME_DIR/.tmux.conf
 ln -s --relative $HOME_DIR/.config/nvim/init.vim $HOME_DIR/.vimrc
 ln -s --relative $HOME_DIR/.config/ideavim/ideavimrc $HOME_DIR/.ideavim
-ln -s --relative $HOME_DIR/.config/X/Xresources $HOME_DIR/.Xresources
 ln -s --relative $HOME_DIR/.config/X/xinitrc $HOME_DIR/.xinitrc
 ln -s --relative $HOME_DIR/.config/git/gitconfig $HOME_DIR/.gitconfig
 
 rm -rf config
 
-# Apply urxvt configuration
-mkdir -p $HOME_DIR/.urxvt/ext
-wget https://raw.githubusercontent.com/majutsushi/urxvt-font-size/master/font-size -O $HOME_DIR/.urxvt/ext/font-size
-
 mkdir $HOME_DIR/.bin
+mkdir $HOME_DIR/.app
 
 # Switch to zsh
 env zsh

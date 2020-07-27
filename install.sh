85411952948534fc2d29651ff55aaad95362b190
Vitalie Mudrenco <xvitcoder@gmail.com>
2020-07-27 12:24:14 +0300

    Configured system services

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ echo "Installing fonts"
 sudo pacman -Syu --noconfirm ttf-dejavu ttf-jetbrains-mono
 
 echo "Installing applications"
-sudo pacman -Syu --noconfirm firefox vimb qutebrowser pcmanfm bluez bluez-utils neovim tmux zsh bat htop git ack ripgrep k9s go go-utils ansible fzf helm ranger tree wget vagrant jdk-openjdk maven gradle libreoffice
+sudo pacman -Syu --noconfirm firefox vimb qutebrowser pcmanfm bluez bluez-utils neovim tmux zsh bat htop git ack ripgrep k9s go go-utils ansible fzf helm ranger tree wget vagrant jdk-openjdk maven gradle libreoffice docker
 
 echo "Install AUR"
 sudo git clone https://aur.archlinux.org/yay-git.git
@@ -30,6 +30,8 @@ CHSH=no RUNZSH=no sh ./install_oh_my_zsh.sh
 rm install_oh_my_zsh.sh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+# Configuration
+# ---------------
 echo "Clonning the config repo"
 git clone https://github.com/xvitcoder/config.git
 mkdir -p $HOME_DIR/.config
@@ -50,9 +52,30 @@ ln -s --relative $HOME_DIR/.config/X/xprofile $HOME_DIR/.xprofile
 ln -s --relative $HOME_DIR/.config/git/gitconfig $HOME_DIR/.gitconfig
 
 rm -rf config
+# ---------------
 
+# Create necessary directories
+# ---------------
 mkdir $HOME_DIR/.bin
 mkdir $HOME_DIR/.app
+# ---------------
+
+# Enable services
+# ---------------
+sudo systemctl enable bluetooth.service
+sudo systemctl start bluetooth.service
+
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+sudo systemctl enable lightdm.service
+sudo systemctl start lightdm.service
+
+sudo systemctl enable systemd-networkd.service
+sudo systemctl start systemd-networkd.service
+# ---------------
 
 # Switch to zsh
+# ---------------
 env zsh
+# ---------------

6e0392c14df8508f97bdb3a24fec982a364a7bad
Vitalie Mudrenco <xvitcoder@gmail.com>
2020-07-27 23:32:53 +0300

    Enahnaced installer

--- install.sh
+++ install.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 echo "Installing Window Manager"
-sh $HOME/.config/install/install-window-manager.sh
+sh install/install-window-manager.sh
 
 echo "Installing System"
-sh $HOME/.config/install/install-audio.sh
-sh $HOME/.config/install/install-bluetooth.sh
-sh $HOME/.config/install/install-printers.sh
-sh $HOME/.config/install/install-laptop.sh
+sh install/install-audio.sh
+sh install/install-bluetooth.sh
+sh install/install-printers.sh
+sh install/install-laptop.sh
 
 echo "Installing Fonts"
-sh $HOME/.config/install/install-fonts.sh
+sh install/install-fonts.sh
 
 echo "Installing Software"
-sh $HOME/.config/install/install-software.sh
-sh $HOME/.config/install/install-software-dev.sh
+sh install/install-software.sh
+sh install/install-software-dev.sh
 
 echo "Installing System"
-sh $HOME/.config/install/install-aur.sh
+sh install/install-aur.sh
 
 # Configuration
 # ---------------
 echo "Configuring System"
-sh $HOME/.config/install/configure-oh-my-zsh.sh
-sh $HOME/.config/install/configure-conf.sh
+sh install/configure-oh-my-zsh.sh
+sh install/configure-conf.sh
 # ---------------
 
 # Create necessary directories

98ef60bedc459d89ff04978da278f8339b22942f
Vitalie Mudrenco <xvitcoder@gmail.com>
2020-08-24 19:18:52 +0300

    Fixed permission after run

--- install/run-after.sh
+++ install/run-after.sh
@@ -9,7 +9,7 @@ source $INSTALL_DIR/utils.sh
 func_print "Applying after configuration" 4
 ###############################################################################
 
-cp $INSTALL_DIR/hid_magicmouse.conf /etc/modprobe.d/hid_magicmouse.conf
+sudo cp $INSTALL_DIR/hid_magicmouse.conf /etc/modprobe.d/hid_magicmouse.conf
 
 mkdir -p ~/.cache/i3lock/current
 betterlockscreen -u /usr/share/backgrounds/archlinux/archlinux-simplyblack.png -b 1.0

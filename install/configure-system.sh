351c3caf30303ea0edcb3287e9fc618d7d1ffe89
Vitalie Mudrenco <xvitcoder@gmail.com>
2020-08-24 12:43:00 +0300

    Added magicmouse support

--- install/configure-system.sh
+++ install/configure-system.sh
@@ -15,6 +15,8 @@ func_print "Applying system configuration" 4
 sudo sed -i 's|#GRUB_THEME=".*"|GRUB_THEME="/boot/grub/themes/arch-silence/theme.txt"|' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
+cp $INSTALL_DIR/hid_magicmouse.conf /etc/modprobe.d/
+
 ###############################################################################
 
 func_print "Configuration Done" 11

7b6164319385b05d2acc35902a874b1919ec37dc
Vitalie Mudrenco <xvitcoder@gmail.com>
2021-03-23 09:49:35 +0200

    Changed the cmus polybar conf

--- polybar/scripts/cmus.sh
+++ polybar/scripts/cmus.sh
@@ -7,14 +7,14 @@ prepend_zero () {
 filename=$(echo -n $(cmus-remote -C status | grep "file"))
 
 if [[ $filename = *[!\ ]* ]]; then
-        song=$(echo -n $(cmus-remote -C status | grep title | cut -c 11-))
+        # song=$(echo -n $(cmus-remote -C status | grep title | cut -c 11-))
         position=$(cmus-remote -C status | grep position | cut -c 10-)
         minutes1=$(prepend_zero $(($position / 60)))
         seconds1=$(prepend_zero $(($position % 60)))
         duration=$(cmus-remote -C status | grep duration | cut -c 10-)
         minutes2=$(prepend_zero $(($duration / 60)))
         seconds2=$(prepend_zero $(($duration % 60)))
-        echo -n "$(basename "$filename") - $song [$minutes1:$seconds1/$minutes2:$seconds2]"
+        echo -n "♬ [$minutes1:$seconds1/$minutes2:$seconds2]"
 else
         echo
 fi
